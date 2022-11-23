8549a6137ed7bdc767f6d2824c32dbf36159f64f
SnoopyKim <snoopykim0@gmail.com>
2022-11-23 22:30:36 +0900

    O 인풋 form 1~3

--- src/components/form/o/index.js
+++ src/components/form/o/index.js
@@ -1,11 +1,16 @@
 import { Box, Typography } from "@mui/material";
+import BodyFat from "./body-fat";
+import Cellular from "./cellular";
+import Hydration from "./hydration";
 
 const FormOView = () => {
-
-    return (
-        <Box>
-        </Box>
-    );
-}
+  return (
+    <Box px={4}>
+      <Cellular />
+      <Hydration />
+      <BodyFat />
+    </Box>
+  );
+};
 
 export default FormOView;
