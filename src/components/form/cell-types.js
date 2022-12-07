0cf4929f8b040b58abcc9e62fbaca60581dfce22
SnoopyKim <snoopykim0@gmail.com>
2022-12-07 16:41:58 +0900

    E Form UI 구현

--- src/components/form/cell-types.js
+++ src/components/form/cell-types.js
@@ -1,4 +1,5 @@
-import { TableCell, Typography } from "@mui/material";
+import { TableCell, TextField, Typography } from "@mui/material";
+import TextInput from "./textinput";
 
 export const BorderedCell = ({ children, ...restProps }) => (
   <TableCell {...restProps} sx={{ border: 1 }}>
@@ -11,8 +12,31 @@ export const EmptyCell = ({ colSpan }) => (
     <Typography> &nbsp;</Typography>
   </BorderedCell>
 );
+
 export const TitleCell = ({ title, colSpan, align }) => (
   <BorderedCell colSpan={colSpan} align={align}>
     <Typography fontWeight={600}>{title}</Typography>
+  </BorderedCell>
+);
+
+export const TextInputCell = ({ type, colSpan, defaultValue, onChange }) => (
+  <BorderedCell colSpan={colSpan}>
+    <TextField
+      variant="standard"
+      fullWidth
+      inputProps={{
+        style: {
+          textAlign: "center",
+        },
+      }}
+      InputProps={{
+        disableUnderline: true,
+      }}
+      maxRows={1}
+      type={type}
+      defaultValue={defaultValue}
+      onChange={(e) => onChange(e.target.value)}
+      onSubmit={() => {}}
+    />
   </BorderedCell>
 );
---
+++ src/components/form/e/digestive.js
@@ -0,0 +1,36 @@
+import { Typography } from "@mui/material";
+import React, { useState } from "react";
+import { theme } from "src/theme";
+import InputForm from "./input-form";
+
+const DigestiveForm = ({ onInput }) => {
+  const [status, setStatus] = useState(null);
+
+  const onChange = (inputValue) => {
+    onInput(inputValue);
+
+    if (50 <= inputValue && inputValue <= 65) {
+      setStatus({ text: "bad", color: theme.palette.score.poor });
+    } else if (66 <= inputValue && inputValue <= 79) {
+      setStatus({ text: "not good", color: theme.palette.score.fair });
+    } else if (80 <= inputValue && inputValue <= 90) {
+      setStatus({ text: "good", color: theme.palette.score.good });
+    } else if (91 <= inputValue && inputValue <= 100) {
+      setStatus({ text: "excellent", color: theme.palette.score.excellent });
+    } else {
+      setStatus(null);
+    }
+  };
+
+  return (
+    <InputForm title={"Digestive Score"} defaultValue={0} onChange={onChange}>
+      {status != null && (
+        <Typography variant="h6" color={status.color}>
+          {status.text}
+        </Typography>
+      )}
+    </InputForm>
+  );
+};
+
+export default DigestiveForm;
